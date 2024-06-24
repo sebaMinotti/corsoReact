3064519bd6bb01f00d1c43c622fb932ad8f90a06
sebaMinotti <seba.minotti@gmail.com>
2024-06-24 17:42:37 +0200

    secondo commit

---
+++ corso/src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../style/Navigation.css'
+function Navigation() {
+  return (
+     <>
+        <div className='container'>
+             <nav>
+             
+             </nav>
+        </div>
+
+     </>
+  )
+}
+
+export default Navigation
