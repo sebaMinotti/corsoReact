24fae3c85b117ca113497725777723f59b92606e
sebaMinotti <seba.minotti@gmail.com>
2024-06-26 17:16:01 +0200

    terzo commit, ho aggiunto un array di icone

--- corso/src/components/Navigation.js
+++ corso/src/components/Navigation.js
@@ -1,11 +1,29 @@
 import React from 'react'
-import '../style/Navigation.css'
+
+import '../style/Navigation.css';
+const icons =[
+   {icona:'fa-brands fa-github',nome:'gitHub',id:1},
+   {icona:'fa-brands fa-linkedin',nome:'linkedin',id:2},
+   {icona:'fa-brands fa-facebook',nome:'facebook',id:3},
+   {icona:'fa-brands fa-instagram',nome:'instagram',id:4},
+]
+
 function Navigation() {
   return (
      <>
         <div className='container'>
              <nav>
-             
+                 <div className='boxSocial'>
+                       {
+                         icons.map(icona=>(
+                             <div className='box' key={icona.id}> 
+                                 <i className={icona.icona}></i>
+                                   <label>{icona.nome}</label>
+                             </div>
+                         ))
+                       }
+                 </div>
+                 
              </nav>
         </div>
 
